9c8118bc357c9abd950ad2f2ab93cb38860fbf7c
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-09-30 13:12:49 +0900

    combination express and socket

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! \n');
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/login', (req, res) => {
+  res.send('<h1>login</h1><a href="chat">chatへのリンク</a>');
 });
+
+const server = http.createServer(app).listen(port);
+
+const io = require("socket.io").listen(server)
+
+const userHash = {}
+
+io.sockets.on("connection", (socket) => {
+
+	// 接続先ユーザーを保存し、他ユーザーへ入室を通知
+	socket.on("connected", (name) => {
+		var msg = name + "が入室しました"
+		userHash[socket.id] = name
+		io.sockets.emit("publish", {value: msg})
+	})
+
+	socket.on("publish", (data) => {
+		io.sockets.emit("publish", {value: data.value})
+	})
+
+	//接続元ユーザーを削除、退室したことを他ユーザーへ通知
+	socket.on("disconnect", () => {
+		if(userHash[socket.id]){
+			var msg = userHash[socket.id] + "が退出しました"
+			delete userHash[socket.id]
+			io.sockets.emit("publish", {value: msg})
+		}
+	})
+})
+
+console.log('Server running at http://127.0.0.1:8080/');
