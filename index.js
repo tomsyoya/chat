b3fcd3757ad26023522fdce061145513418dadfa
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2018-10-24 08:32:51 +0900

    【TMP】

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const ejs  = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
@@ -9,15 +10,28 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/rooms', (req, res) => {
+
+  var data = {
+          rooms: [
+              {name: "<h1>リンゴ</h1>"},
+              {name: "<h2>バナナ</h2>"},
+              {name: "<h3>スイカ</h3>"}
+          ]
+      };
+  res.render("./rooms.ejs", data);
+
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+  res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(__dirname + '/register.html');
+  res.sendFile(__dirname + '/views/register.html');
 });
 
 app.use(bodyParser());
@@ -43,7 +57,7 @@ app.post('/register', function(req, res,next){
         client.close();
     });
   });
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.post('/login', function(req, res, next){
@@ -56,12 +70,12 @@ app.post('/login', function(req, res, next){
     const db = client.db('chat');
 
     // コレクションの取得
-    var collection = db.collection("chat");
+    var collection = db.collection("users");
 
     // ユーザを取得
     collection.find({name: name,password: password}).toArray((error, documents)=>{
 	  if (!documents || documents.length === 0) {
-		  res.redirect('/login?login_error')
+		  res.redirect('/views/login?login_error')
 	  }
       for (var document of documents) {
           var name = document.name;
@@ -71,11 +85,11 @@ app.post('/login', function(req, res, next){
           if (userExists) {
               //ユーザが存在する
               console.log("ユーザが存在する");
-              res.sendFile(__dirname + '/index.html');
+              res.sendFile(__dirname + '/views/index.html');
           } else {
              //ユーザが存在しない
              console.log("ユーザが存在しない");
-             res.sendFile(__dirname + '/register.html');
+             res.sendFile(__dirname + '/views/register.html');
           }
         }
     });
@@ -83,6 +97,36 @@ app.post('/login', function(req, res, next){
   });
 
 });
+
+app.post('/rooms', function(req, res, next){
+
+  var data = {
+          rooms: [
+              {room: "<h1>リンゴ</h1>"},
+              {room: "<h2>バナナ</h2>"},
+              {room: "<h3>スイカ</h3>"}
+          ]
+      };
+  res.render("./rooms.ejs", data);
+  // MongoDB へ 接続
+  //client.connect((error, client) => {
+
+  //  const db = client.db('chat');
+
+    // コレクションの取得
+  //  var collection = db.collection("rooms");
+
+  //  collection.find().toArray((error, rooms)=>{
+  //	  if (!rooms || rooms.length === 0) {
+  		//  res.redirect('/views/login?login_error')
+  //	  }else{
+  //      res.render("./rooms.ejs", data);
+      //  res.render('/views/rooms',rooms);
+  //    }
+  //  });
+  //});
+});
+
 
 
 const server = http.createServer(app).listen(port);
